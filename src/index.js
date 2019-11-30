0b42db5915e1fde2c5ec8d9fa99a7177081b8b8e
RickyYhQi <rickyyuhaoqi@gmail.com>
2019-11-30 13:10:45 +1100

    jsx tag

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+// 如果我们要使用自己创建的组件 直接使用标签名字， 组件开头必须要大写
 ReactDOM.render(<App />, document.getElementById('root'));
 //jsx syntax must import react
 // If you want your app to work offline and load faster, you can change
